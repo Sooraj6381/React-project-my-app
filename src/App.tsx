3abb8aa08f0703a5c4c581e97e2641951dc380ab
Sooraj6381 <sooraj63814@gmail.com>
2026-06-02 22:46:18 +0530

    Updated heading text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
+import { faScissors } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
   return (
     <>
-     <h1>MADRAS SALOON</h1>
+    <div className='heading1'>
+     <FontAwesomeIcon icon={faScissors} className='font1'/>
+     <span><h1>MADRAS</h1></span>
+     <span><h1 >SALOON</h1></span>
+     </div>
     </>
   )
 }
